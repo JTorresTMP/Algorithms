612f9711e6f1b088e6b7e6a7fd878f004dc749d0
Jose Torres <torresjose6541@gmail.com>
2022-02-04 16:30:53 -0800

    largest palindrome, brute force done

--- TypeScript/Euler/004_largestPalindromeProduct.ts
+++ TypeScript/Euler/004_largestPalindromeProduct.ts
@@ -13,3 +13,34 @@ const isPalindrome = (input: number | string): boolean => {
 console.log(isPalindrome(343));
 
 // This sounds like it needs a recursive solution
+
+// Max 3 Digit Number === 999, need to work backwards
+
+const calculateProducts = (n: number) => {
+    const productPalindromeArray: number[] = [];
+    const cache = new Set();
+
+    outer: for (let i = n; i > 0; i--) {
+        // productArray.push(i * n)
+        inner: for (let j = n; j > 0; j--) {
+            const product: number = i * j;
+            if (cache.has(product)) {
+                continue inner;
+            } else {
+                cache.add(product)
+                // console.log(`${i} x ${j} is ${product}`)
+                if (isPalindrome(product)) {
+                    productPalindromeArray.push(product)
+                }
+            }
+
+        }
+    }
+
+    console.log(productPalindromeArray)
+    // sorted in desc order
+    const sortedProductArray: number[] = productPalindromeArray.sort((a, b) => b - a);
+    return sortedProductArray[0];
+}
+
+console.log(calculateProducts(999))
