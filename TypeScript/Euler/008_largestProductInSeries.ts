a8df4bf3ddbeb2ee39f69ebe2166ab0eaeebde26
Jose Torres <torresjose6541@gmail.com>
2022-02-22 18:21:24 -0800

    taking a break, but this isn't working yet

--- TypeScript/Euler/008_largestProductInSeries.ts
+++ TypeScript/Euler/008_largestProductInSeries.ts
@@ -28,13 +28,15 @@ Find the thirteen adjacent digits in the 1000-digit number that have the greates
 // Adjacent numbers are defined as numbers where the delta between them does not
 // exceed 1. E.g. 7, 8, 8, 7, 8
 
+const massiveInput = `7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450`
+
 const isAdjacentNumber = (number1: number, number2: number) => {
     return Math.abs(number1 -  number2) <= 1;
 }
 
-const testInput = 556938874;
+const testInput = 7316717653133062491922511967442657474235534919493496983520312774506326239;
 
-const findAdjacentNumbers = (length: number, series: number) => {
+const findAdjacentNumbers = (length: number, series: number | string) => {
     const sequences: number[][] = [];
     const stringSeries = series.toString();
     let leftPointer = 0;
@@ -42,14 +44,15 @@ const findAdjacentNumbers = (length: number, series: number) => {
 
     for (let i = leftPointer; i < stringSeries.length; i++) {
         const localSequence: number[] = [];
-        console.log(`Current Index: ${i}`)
-        console.log(`
-        Current Left: ${leftPointer},
-        Current Right: ${rightPointer}
-        `)
+        // console.log(`Current Index: ${i}`)
+        // console.log(`
+        // Current Left: ${leftPointer},
+        // Current Right: ${rightPointer}
+        // `)
         while (isAdjacentNumber(parseInt(stringSeries[leftPointer]), parseInt(stringSeries[rightPointer]))) {
             localSequence.push(parseInt(stringSeries[leftPointer]));
             leftPointer++
+            // rightPointer = leftPointer + 1;
         }
         leftPointer = i + 1;
         rightPointer = leftPointer + 1;
@@ -58,7 +61,7 @@ const findAdjacentNumbers = (length: number, series: number) => {
         localSequence.length > 1 ? sequences.push(localSequence) : null; // do nothing
     }
 
-    console.log('All Sequences')
+    // console.log('All Sequences')
     console.log(sequences)
 
     const validLengthSequences = sequences.filter((localArr: number[]) => localArr.length === length)
@@ -67,4 +70,4 @@ const findAdjacentNumbers = (length: number, series: number) => {
 }
 
 
-console.log(findAdjacentNumbers(3, testInput))
+console.log(findAdjacentNumbers(7, massiveInput))
