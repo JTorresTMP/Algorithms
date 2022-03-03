11d488c174186be2d71d0e5297eff99d133759b7
Jose Torres <torresjose6541@gmail.com>
2022-03-02 22:25:54 -0800

    pushing for streak only

--- TypeScript/Euler/009_specialPythagoreanTriplet.ts
+++ TypeScript/Euler/009_specialPythagoreanTriplet.ts
@@ -7,3 +7,5 @@ For example, 3**2 + 4**2 = 9 + 16 = 25 = 5**2.
 There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 Find the product abc.
 */
+
+// pushing before I forget - have to fix qa db
