b40fddabb0565746f481952f859b9801a66953bc
Jose Torres <torresjose6541@gmail.com>
2022-04-28 22:05:40 -0700

    removing superfluous comments

--- TypeScript/Euler/009_specialPythagoreanTriplet.ts
+++ TypeScript/Euler/009_specialPythagoreanTriplet.ts
@@ -13,17 +13,3 @@ First thing I need to do is identify all pythagorean triplets until sum is equal
 
 Once I find those I just need to multiply them
 */
-// won't have time, rds upgrades
-// earliest push so far? 
-
-// I need to get on this
-
-// testing personal access tokens
-
-// is it cached now?
-
-// super quick
-
-// going to get back to this soon
-
-// actual migration tonight
