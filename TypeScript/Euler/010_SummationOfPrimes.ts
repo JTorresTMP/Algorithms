f4df667ab41b1f00f1d8f5b8e30a3f669d8b7609
Jose Torres <torresjose6541@gmail.com>
2022-05-04 18:39:45 -0700

    not working atm

---
+++ TypeScript/Euler/010_SummationOfPrimes.ts
@@ -0,0 +1,41 @@
+/*
+The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17
+
+Find the sum of all primes below two million.
+*/
+import { isPrime } from "./003_largestPrimeFactors";
+
+const recursiveIsPrime = (n: number) => {
+    if (n <= 1) return false;
+    if (n === 2) return true;
+
+    let num = Math.sqrt(n);
+
+    for (let i = 2; i < num; i++) {
+        if (num % i === 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+const summationOfPrimes = () => {
+    const upperLimit = 2000000;
+    const primes: number[] = [];
+
+    for (let i = 1; i < upperLimit; i++) {
+        if (recursiveIsPrime(i)) {
+            primes.push(i)
+        }
+    }
+
+    console.log(primes);
+    const answer = primes.reduce((agg, curr) => agg + curr, 0);
+
+    return answer;
+}
+
+console.log(summationOfPrimes());
+
+// This does not work currently
